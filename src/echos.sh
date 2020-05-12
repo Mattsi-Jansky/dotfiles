132c720b49ae2e8db3973d6e87024607d57a3f08
Mattsi <mattsi.jansky@codurance.com>
2020-05-12 16:08:17 +0100

    Fix missing output

--- src/echos.sh
+++ src/echos.sh
@@ -16,7 +16,7 @@ COL_MAGENTA=$ESC_SEQ"35;01m"
 COL_CYAN=$ESC_SEQ"36;01m"
 
 function ok() {
-    echo -e "$COL_GREEN[ok]$COL_RESET "$1
+    echo -e "$COL_GREEN [ok] $COL_RESET "$1
 }
 
 function running() {
@@ -24,13 +24,13 @@ function running() {
 }
 
 function action() {
-    echo -e "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
+    echo -e "\n$COL_YELLOW [action]: $COL_RESET\n ⇒ $1..."
 }
 
 function warn() {
-    echo -e "$COL_YELLOW[warning]$COL_RESET "$1
+    echo -e "$COL_YELLOW [warning] $COL_RESET "$1
 }
 
 function error() {
-    echo -e "$COL_RED[error]$COL_RESET "$1
+    echo -e "$COL_RED [error] $COL_RESET "$1
 }
